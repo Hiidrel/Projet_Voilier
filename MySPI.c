cacf20eb0a6acd4f6254c311e5d6d96cb10a0d91
Valentin Guittard <103254595+Hiidrel@users.noreply.github.com>
2022-12-01 16:21:58 +0100

    Add files via upload

--- MySPI.c
+++ MySPI.c
@@ -9,42 +9,52 @@
 					 - /NSS (/CS) : Out push pull
 	* @param SPI_TypeDef * SPI : SPI1 ou SPI2 
   */
-
-void MySPI_Init(SPI_TypeDef * SPI){
-	/* config des modes des pins */
+	
+	/* init des 4 sorties de l'ADXL */
 	MyGPIO_Struct_TypeDef NSS={GPIOA, 4, Out_Ppull};
 	MyGPIO_Struct_TypeDef SCK={GPIOA, 5, AltOut_Ppull};
 	MyGPIO_Struct_TypeDef MISO={GPIOA, 6, In_Floating};
   MyGPIO_Struct_TypeDef MOSI={GPIOA, 7, AltOut_Ppull}; 
+
+void MySPI_Init(SPI_TypeDef * SPI){
+	SPI->CR1|=1<<2; //master mode 
+	/* enable de la clock et remap de SPI1 */
+	RCC->APB2ENR|=RCC_APB2ENR_SPI1EN;
+	AFIO->MAPR&=~(AFIO_MAPR_SPI1_REMAP);
+	/* init de BaudRate à FSCK=281kHz, replace NSS by SSI, SSI to determine NSS, SSM to manage slave, SPE to enable the SPI */
+	SPI->CR1|=SPI_CR1_BR;
+	SPI->CR1|=SPI_CR1_SSM;
+	SPI->CR1|=SPI_CR1_SSI;
+	SPI->CR1|=SPI_CR1_MSTR;
+	SPI->CR1|=SPI_CR1_SPE;
+	/* init de la lecture du slave sur front montant = transmission mode */
+	SPI->CR1|=SPI_CR1_CPHA;
+	SPI->CR1|=SPI_CR1_CPOL;
+	
+	
+	SPI->CR1&=~(SPI_CR1_LSBFIRST);
+	SPI->CR1&=~(SPI_CR1_RXONLY);
+	SPI->CR1&=~(SPI_CR1_DFF);
+	
+	//	/* init du SR */
+	//	SPI->SR&=~(SPI_SR_BSY); //not busy when it starts
+	//	SPI->SR|=(SPI_SR_TXE); // transmission buffer empty
+	//	SPI->SR&=~(SPI_SR_RXNE); //reception buffer empty
+	
+	/* config des modes des pins */
 	RCC->APB2ENR|=RCC_APB2ENR_IOPAEN; //enable de la clock
 	MyGPIO_Init(&NSS);
 	MyGPIO_Init(&SCK);
 	MyGPIO_Init(&MISO);
 	MyGPIO_Init(&MOSI);	
-	/* enable de la clock et remap de SPI1 */
-	RCC->APB2ENR|=RCC_APB2ENR_SPI1EN;
-	AFIO->MAPR&=~(AFIO_MAPR_SPI1_REMAP);
-	/* init de BaudRate à FSCK=281kHz, replace NSS by SSI, SSI to determine NSS, SSM to manage slave, SPE to enable the SPI */
-	SPI->CR1|=SPI_CR1_BR+SPI_CR1_SSM+SPI_CR1_SSI+SPI_CR1_MSTR+SPI_CR1_SPE;
-	/* init de la lecture du slave sur front montant */
-	SPI->CR1|=SPI_CR1_CPHA+SPI_CR1_CPOL;
-	SPI->CR1&=~(SPI_CR1_LSBFIRST);
-	SPI->CR1&=~(SPI_CR1_RXONLY);
-	SPI->CR1&=~(SPI_CR1_DFF);
-	/* init du SR */
-	SPI->SR&=~(SPI_SR_BSY); //not busy when it starts
-	SPI->SR|=(SPI_SR_TXE); // transmission buffer empty
-	SPI->SR&=~(SPI_SR_RXNE); //reception buffer empty
 }
 
 void MySPI_Send(char ByteToSend){
-	while(!(SPI1->SR&(SPI_SR_TXE))){};
+	//while(!(SPI1->SR&(SPI_SR_TXE))){};
 	SPI1->DR = ByteToSend;
 }
 
 char MySPI_Read(void){
-	while ((SPI1->SR)&SPI_SR_BSY){};
-	SPI1->DR=0; //useless data
 	while((SPI1->SR&(SPI_SR_RXNE))){};
 	return SPI1->DR;
 }
--- Roulis.c
+++ Roulis.c
@@ -1,70 +1,90 @@
 #include <MySPI.h>
 #include <math.h>
 #include <Driver_GPIO.h>
+#include <Roulis.h>
 
-#define ADXL345_MB 0x40
-#define ADXL345_READ 0x80
-#define ADXL345_POWER_CTL 0x2D
-#define ADXL345_BW_RATE 0x2C
-#define ADXL345_DATA_FORMAT 0x31
-#define M_PI 3.1415
-uint16_t mesx;
-uint16_t mesy;
-uint16_t mesz;
-signed int accx;
-signed int accy;
-signed int accz;
-int roll;
-int redress;
-
-int calculRoulis(){
-		/* récup des valeurs sur les axes y et z */
+void calculRoulis(Acc * ptrAcc){
+	
+		/* variables pour stocker DATA_X, DATA_Y et DATA_Z */
+		int mesx0;
+		int mesx1;
+		int mesy0;
+		int mesy1;
+		int mesz0;
+		int mesz1;
+	
+		/* récup des valeurs sur les axes x y et z */
 		MySPI_Clear_NSS();
-		MySPI_Send(0x32|ADXL345_MB|ADXL345_READ);
-		mesx = MySPI_Read();
-		mesx += MySPI_Read()<<8;
-		mesx &=~(0xE000);
-		mesy = MySPI_Read();
-		mesy += MySPI_Read()<<8;
-		mesx &=~(0xE000);
-		mesz=MySPI_Read();
-		mesz += MySPI_Read()<<8;
-		mesx &=~(0xE000);
+		MySPI_Send(DATA_START|ADXL345_MB|ADXL345_READ);
+		mesx0=MySPI_Read();
+		mesx1=MySPI_Read();
+		mesy0=MySPI_Read();
+		mesy1=MySPI_Read();
+		mesz0=MySPI_Read();
+		mesz1=MySPI_Read();
 		MySPI_Set_NSS();
-		/* calcul de l'angle de rotation du voilier */
-		accx = (signed int)((mesx) * 3.9);
-		accy = (signed int)((mesy) * 3.9);
-		accz = (signed int)((mesz) * 3.9);
-		roll = (180 * atan (accy/sqrt(accx*accx + accz*accz)))/M_PI;
-		
-		/** ci-après le code permettant d'appeler la fonction retourRoulis pour controleVoile (cf. programme voiles) --> pour redresser le voilier
-		if (roll > seuil){ //seuil valeur empirique que nous n'avons pas pu déterminer
-			redress=1;
-		}
-		else{
-		**/
-		redress=0; //renvoie toujours false (cf. pas de test roll > seuil)
-		
-		//}
-		return redress;
+	
+		/* reset puis mise à jour du pointeur d'accélération */
+		ptrAcc->accx = ptrAcc->accx &~(0xFF);
+		ptrAcc->accx = ptrAcc->accx &~(0xFF<<8);
+		ptrAcc->accy = ptrAcc->accy &~(0xFF);
+		ptrAcc->accy = ptrAcc->accy &~(0xFF<<8);
+		ptrAcc->accz = ptrAcc->accz &~(0xFF);
+		ptrAcc->accz = ptrAcc->accz &~(0xFF<<8);
+		ptrAcc->accx = ptrAcc->accx | mesx0;
+		ptrAcc->accx = ptrAcc->accx |(mesx1<<8);
+		ptrAcc->accy = ptrAcc->accy | mesy0;
+		ptrAcc->accy = ptrAcc->accy |(mesy1<<8);
+		ptrAcc->accz = ptrAcc->accz | mesz0;
+		ptrAcc->accz = ptrAcc->accz |(mesz1<<8);
 }
 
 void initRoulis (void){
-	
-	MySPI_Init(SPI1);
 	MySPI_Clear_NSS();
 	/* set de POWER_CTL tq Measure=1 */
 	MySPI_Send(ADXL345_POWER_CTL& ~(ADXL345_READ));
 	MySPI_Send(0x8);
-	/* set de BW_RATE tq freq=100 */
+	/* set de BW_RATE tq freq=100Hz */
 	MySPI_Send(ADXL345_BW_RATE& ~(ADXL345_READ));
 	MySPI_Send(0xA);
 	/* set de DATA_FORMAT à 0 (right alignement of bytes)*/
 	MySPI_Send(ADXL345_DATA_FORMAT& ~(ADXL345_READ));
-	MySPI_Send(0x0);
+	MySPI_Send(0xB);
 	MySPI_Set_NSS();
 }
 
+Acc acceleration; 
+float X;
+float Y;
+float Z;
+float roll;
+int redress = 0;
 int main (void){
-	while(1){};
+	/*initialisation du roulis et des différentes variables */
+	roll=0.0;
+	acceleration.accx=0;
+	acceleration.accy=0;
+	acceleration.accz=0;
+	MySPI_Init(SPI1);
+	initRoulis();
+	
+	/* boucle infinie : récupération des données de l'accélération du voilier */
+	while(1){
+		calculRoulis(&acceleration);
+		
+		/* calcul de roll et mise à jour de redress -> notifier un redressement */
+		X=acceleration.accx * 4e-3;
+		Y=acceleration.accy * 4e-3;
+		Z=acceleration.accz * 4e-3;
+		if (Y*Z<0){
+			roll=atan(-Y/Z);
+		} else {
+			roll=atan(Y/Z);
+		}
+		if (roll>=(40*2*M_PI/360)){ //limite de 40° en radians
+			redress = 1;
+		} else { 
+			redress = 0;
+		}
+	};
 }
